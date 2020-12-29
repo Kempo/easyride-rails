61f7d34066efaf3d632602ebe98e0852d0277eba
Aaron <ilestkempo@gmail.com>
2020-12-28 19:54:15 -0800

    fixed ordering of preferences

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,12 +1,8 @@
-# query to organize riders and drivers (manually clear cars + reassign?)
-# find a way to keep state of current assignments before/after changes
-# mutations to CRUD riders and drivers and their preferences
-
 class Driver < Person
   has_one :car, inverse_of: :driver, dependent: :destroy
   delegate :riders, :riders_count, :clear_space, to: :car
 
-  has_many :associations, class_name: 'Preference', as: :preferrer, dependent: :destroy
+  has_many :associations, -> { order(created_at: :ASC) }, class_name: 'Preference', as: :preferrer, dependent: :destroy
   has_many :preferences, through: :associations, source: :preferable, source_type: 'Rider'
 
   after_create :create_car!
--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,8 +1,8 @@
 class Rider < Person
   belongs_to :car, inverse_of: :riders, optional: true, counter_cache: true
  
-  has_many :associations, class_name: 'Preference', as: :preferrer, dependent: :destroy
-  has_many :preferences, -> { order(created_at: :asc) }, through: :associations, source: :preferable, source_type: 'Driver'
+  has_many :associations, -> { order(created_at: :ASC) }, class_name: 'Preference', as: :preferrer, dependent: :destroy
+  has_many :preferences, through: :associations, source: :preferable, source_type: 'Driver'
 
   scope :unassigned, -> { where(car: nil) }
 
